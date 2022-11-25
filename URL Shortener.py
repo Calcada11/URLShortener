3ba1d2ab9a36c4f68f40e2e34bc0db08978ff780
Gabriel Calçada <gmcalcada@gmail.com>
2022-11-24 23:29:36 -0300

    Added comments

--- URL Shortener.py
+++ URL Shortener.py
@@ -1,7 +1,11 @@
+"Simple enough, URL gets typed in, URL gets shortened"
+
 import pyshorteners
 
+#Address input
 long_url = input("Enter URL to be shortened: ")
 
+#Conversion of address
 type_bitly = pyshorteners.Shortener(api_key = 'd1fbab372d3735b98a814c460c7d73fda58adacc')
 short_url = type_bitly.bitly.short(long_url)
 
