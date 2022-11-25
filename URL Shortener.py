c0bf3afefd15a2cefe2104b9e9707e71fbd8379a
Gabriel Calçada <gmcalcada@gmail.com>
2022-11-24 23:21:12 -0300

    Data input correction

--- URL Shortener.py
+++ URL Shortener.py
@@ -3,6 +3,6 @@ import pyshorteners
 long_url = input("Enter URL to be shortened: ")
 
 type_bitly = pyshorteners.Shortener(api_key = 'd1fbab372d3735b98a814c460c7d73fda58adacc')
-short_url = type_bitly.bitly.short('https://www.google.com')
+short_url = type_bitly.bitly.short(long_url)
 
 print("Shortened link is here! " + short_url)
