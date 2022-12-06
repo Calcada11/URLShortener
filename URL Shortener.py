19a09223eec874587b0fde9337382e133ce256c8
Gabriel Calçada <gmcalcada@gmail.com>
2022-12-06 16:47:50 -0300

    feat: Copy shortened URL to clipboard

--- URL Shortener.py
+++ URL Shortener.py
@@ -1,5 +1,6 @@
 "Simple enough, URL gets typed in, URL gets shortened"
 
+import pyperclip
 import pyshorteners
 
 #Address input
@@ -9,4 +10,8 @@ long_url = input("Enter URL to be shortened: ")
 type_bitly = pyshorteners.Shortener(api_key = 'd1fbab372d3735b98a814c460c7d73fda58adacc')
 short_url = type_bitly.bitly.short(long_url)
 
-print("Shortened link is here! " + short_url)
+#Copy link to clipboard
+pyperclip.copy(short_url)
+
+#Print new (shortened) URL
+print("Shortened link is here: " + short_url)
